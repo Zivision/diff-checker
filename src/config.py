ac95539f3cd46d592c09db0b75625969e6610e9b
Zivision <zcastro703@gmail.com>
2026-03-23 13:44:01 -0500

    feat: parse_conf

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tomllib
+import re
 
 
 # Default path for config file
@@ -33,3 +34,13 @@ def load_conf(path: str) -> dict[str, dict[str, str]]:
         )
     with open(conf_path, "rb") as f:
         return tomllib.load(f)
+
+
+def parse_conf(conf: dict[str, dict[str, str]]):
+    for key in conf.keys():
+        if "git_project" in conf[key]:
+            print(key)
+            print("Bing")
+        if "flake_project" in conf[key]:
+            print(key)
+            print("Bong")
